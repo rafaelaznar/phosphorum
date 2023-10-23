a8eacc73013913ee9b64f2f8df47c6da0e3e8493
PabloMolero <pmoleromarin@gmail.com>
2023-10-23 12:12:30 +0200

    prueba form

--- src/app/components/user/admin-user-form-unrouted/admin-user-form-unrouted.component.ts
+++ src/app/components/user/admin-user-form-unrouted/admin-user-form-unrouted.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-admin-user-form-unrouted',
@@ -6,12 +7,29 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./admin-user-form-unrouted.component.css']
 })
 export class AdminUserFormUnroutedComponent implements OnInit {
+  userForm: FormGroup;
 
   @Input() id: number = 1;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      // Aquí puedes enviar los datos del usuario al servidor o realizar otras acciones necesarias.
+      console.log(this.userForm.value);
+    }
   }
 
 }
