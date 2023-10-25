16d10fc1e1bb36bb772abf852332dd7475b620cf
AlanMcLure <alanmclurealarcon02@gmail.com>
2023-10-25 11:46:02 +0200

    admin-reply-detail-unrouted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { AdminUserEditRoutedComponent } from './components/user/admin-user-edit-
 import { AdminUserPlistUnroutedComponent } from './components/user/admin-user-plist-unrouted/admin-user-plist-unrouted.component';
 import { AdminUserDetailUnroutedComponent } from './components/user/admin-user-detail-unrouted/admin-user-detail-unrouted.component';
 import { AdminUserFormUnroutedComponent } from './components/user/admin-user-form-unrouted/admin-user-form-unrouted.component';
+import { AdminReplyDetailUnroutedComponent } from './components/reply/admin-reply-detail-unrouted/admin-reply-detail-unrouted.component';
 //--
 @NgModule({
   declarations: [
@@ -46,6 +47,7 @@ import { AdminUserFormUnroutedComponent } from './components/user/admin-user-for
     AdminUserDetailUnroutedComponent,
     AdminUserFormUnroutedComponent,
     //--
+    AdminReplyDetailUnroutedComponent
   ],
   imports: [
     BrowserModule,
