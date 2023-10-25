2e1810ae05f2057aee33cd71ecb16bdb7db4e87a
piol14 <eof812@gmail.com>
2023-10-25 11:52:34 +0200

    He subido el admin-thread-form-unrouted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { AdminUserEditRoutedComponent } from './components/user/admin-user-edit-
 import { AdminUserPlistUnroutedComponent } from './components/user/admin-user-plist-unrouted/admin-user-plist-unrouted.component';
 import { AdminUserDetailUnroutedComponent } from './components/user/admin-user-detail-unrouted/admin-user-detail-unrouted.component';
 import { AdminUserFormUnroutedComponent } from './components/user/admin-user-form-unrouted/admin-user-form-unrouted.component';
+import { AdminThreadFormUnroutedComponent } from './components/thread/admin-thread-form-unrouted/admin-thread-form-unrouted.component';
 //--
 @NgModule({
   declarations: [
@@ -46,6 +47,7 @@ import { AdminUserFormUnroutedComponent } from './components/user/admin-user-for
     AdminUserDetailUnroutedComponent,
     AdminUserFormUnroutedComponent,
     //--
+    AdminThreadFormUnroutedComponent
   ],
   imports: [
     BrowserModule,
