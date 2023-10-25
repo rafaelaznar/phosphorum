523e7a6d3c8a6bb6620ae3120d122c0abaeee287
lucas <lucgr04.vlc@gmail.com>
2023-10-25 11:33:12 +0200

    Hecho reply-plist-routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { AdminUserViewRoutedComponent } from './components/user/admin-user-view-
 import { AdminUserPlistRoutedComponent } from './components/user/admin-user-plist-routed/admin-user-plist-routed.component';
 import { AdminUserEditRoutedComponent } from './components/user/admin-user-edit-routed/admin-user-edit-routed.component';
 import { AdminUserNewRoutedComponent } from './components/user/admin-user-new-routed/admin-user-new-routed.component';
+import { AdminReplyPlistRoutedComponent } from './components/reply/admin-reply-plist-routed/admin-reply-plist-routed.component';
 //--
 const routes: Routes = [
   { path: '', component: HomeRoutedComponent },
@@ -15,7 +16,7 @@ const routes: Routes = [
   { path: 'admin/user/view/:id', component: AdminUserViewRoutedComponent },  
   { path: 'admin/user/edit/:id', component: AdminUserEditRoutedComponent },
   { path: 'admin/user/new', component: AdminUserNewRoutedComponent },
-
+  { path: 'admin/reply/plist', component: AdminReplyPlistRoutedComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { AdminUserEditRoutedComponent } from './components/user/admin-user-edit-
 import { AdminUserPlistUnroutedComponent } from './components/user/admin-user-plist-unrouted/admin-user-plist-unrouted.component';
 import { AdminUserDetailUnroutedComponent } from './components/user/admin-user-detail-unrouted/admin-user-detail-unrouted.component';
 import { AdminUserFormUnroutedComponent } from './components/user/admin-user-form-unrouted/admin-user-form-unrouted.component';
+import { AdminReplyPlistRoutedComponent } from './components/reply/admin-reply-plist-routed/admin-reply-plist-routed.component';
 //--
 @NgModule({
   declarations: [
@@ -46,6 +47,7 @@ import { AdminUserFormUnroutedComponent } from './components/user/admin-user-for
     AdminUserDetailUnroutedComponent,
     AdminUserFormUnroutedComponent,
     //--
+    AdminReplyPlistRoutedComponent,
   ],
   imports: [
     BrowserModule,
