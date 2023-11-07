7bb283cb8e8d6b3891480add1ed4a3ba148a2547
Rafael Aznar <rafaaznar@gmail.com>
2023-11-07 08:55:13 +0100

    mejora login

--- src/app/components/shared/login-routed/login-routed.component.ts
+++ src/app/components/shared/login-routed/login-routed.component.ts
@@ -1,6 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { SessionAjaxService } from 'src/app/service/session.ajax.service.ts.service';
 
 @Component({
@@ -8,6 +10,7 @@ import { SessionAjaxService } from 'src/app/service/session.ajax.service.ts.serv
   templateUrl: './login-routed.component.html',
   styleUrls: ['./login-routed.component.css']
 })
+
 export class LoginRoutedComponent implements OnInit {
 
   loginForm: FormGroup;
@@ -15,7 +18,9 @@ export class LoginRoutedComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private oSessionService: SessionAjaxService
+    private oSessionService: SessionAjaxService,
+    private oMatSnackBar: MatSnackBar,
+    private oRouter: Router,
   ) {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required]],
@@ -31,9 +36,12 @@ export class LoginRoutedComponent implements OnInit {
       this.oSessionService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe({
         next: (data: any) => {
           this.oSessionService.setToken(data);
+          this.oMatSnackBar.open("Loggin successfull.", '', { duration: 2000 });
+          this.oRouter.navigate(['/home']);
         },
         error: (error: HttpErrorResponse) => {
           this.status = error;
+          this.oMatSnackBar.open("Error in loggin operation.", '', { duration: 2000 });
         }
       });
     }
@@ -41,6 +49,20 @@ export class LoginRoutedComponent implements OnInit {
 
   onReset() {
     this.loginForm.reset();
+  }
+
+  loginAdmin() {
+    this.loginForm.setValue({
+      username: 'pedropicapiedra',
+      password: 'e2cac5c5f7e52ab03441bb70e89726ddbd1f6e5b683dde05fb65e0720290179e'
+    })
+  }
+
+  loginUser() {
+    this.loginForm.setValue({
+      username: 'pablomarmol',
+      password: 'e2cac5c5f7e52ab03441bb70e89726ddbd1f6e5b683dde05fb65e0720290179e'
+    })
   }
 
 }
