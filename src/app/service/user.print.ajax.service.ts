b603d244e96ff1071f7508c7d098e3553701b40e
Nieves <smilenvs@gmail.com>
2023-12-13 18:44:59 +0100

    Dejandolo bonito

--- src/app/service/user.print.ajax.service.ts
+++ src/app/service/user.print.ajax.service.ts
@@ -13,8 +13,7 @@ export class UserPrintAjaxService {
 
 
   constructor(
-    private oUserAjaxService: UserAjaxService,
-    private oThreadAjaxService: ThreadAjaxService
+    private oUserAjaxService: UserAjaxService
   ) { }
 
 
@@ -28,9 +27,13 @@ export class UserPrintAjaxService {
         console.log('User to print:', oUserToPrint);
         //Cosas
         var doc = new jsPDF();
-        doc.setFont('Courier');
+        doc.setFont('Arial');
+        doc.setFontSize(12);
         //doc.text(50, 64, `Prueba de si funciona`);
-        doc = this.cabecera(doc, oUserToPrint);
+        doc = this.cabecera(doc);
+        doc = this.contenido(doc, oUserToPrint);
+        doc = this.pie(doc);
+
 
         doc.save('prueba.pdf');
       }
@@ -38,53 +41,134 @@ export class UserPrintAjaxService {
   }
 
 
-  private cabecera(doc: any, oUserToPrint: IUser): any {
-    const baseX = 10;
+  private cabecera(doc: any): any {
+
+    const indigoPastel = "#7887AB";
+
     doc.setFontType('bold');
     doc.setFontSize(20);
-    doc.text('U s u a r i o', 80, 30);
+    doc.text('PHOSPHORUM 2023', 70, 25);
+    //
+    doc.setDrawColor(indigoPastel);
+    doc.line(60, 30, 145, 30);
+    //
+    doc.text('User\'s Profile', 80, 40);
+    doc.setDrawColor(indigoPastel);
+    doc.line(10, 45, 200, 45);
     doc.setFontType('normal');
-    //
-    doc.setFillColor(240, 240, 240);
-    //separacion de cajas: h=15 v=5
-    doc.rect(baseX, 35, 105, 35, 'F');
-    //
-    /* doc.setFillColor(240, 240, 240);
-     doc.rect(120, 35, 80, 15, 'F');
-     doc.setFontSize(12);
-     doc.text(142, 44, `Id de Usuario: ${oUserToPrint.id}`);
-    */
-    //
-     doc.setFillColor(240, 240, 240);
-     doc.rect(120, 55, 80, 15, 'F');
-     doc.setFontSize(12);
-     doc.text(140, 64, `Nombre: ${oUserToPrint.name}`);
-    //
-    doc.setFillColor(240, 500, 240);
-     doc.rect(120, 35, 80, 15, 'F');
-     doc.setFontSize(12);
-     doc.text(140, 44, `Prueba: ${oUserToPrint.threads}`);
-    
-    //
-    
-    doc.text(50, 64, `Prueba de si funciona: `);
 
-    //Hay que gestionar esto
-   /* var oUserThreads = this.oThreadAjaxService.getPage(undefined, undefined, "defaultOrderField", "defaultOrderDirection", oUserToPrint.id)
-    .subscribe({
-        next: (result: IThreadPage) => {
-            // Hacer algo con los resultados
-            console.log(result);
-        },
-        error: (error) => {
-            // Manejar el error aquí
-            console.error("Error al obtener los hilos del usuario:", error);
-        },
-        complete: () => {
-            // Lógica a ejecutar cuando la operación está completa (si es necesario)
-        }
-    } as Observer<IThreadPage>);*/
-    
+    return doc;
+  }
+
+  private contenido(doc: any, oUserToPrint: IUser): any {
+    const baseX = 10;
+    const indigoPastel = "#7887AB";
+
+    doc.setTextColor(0, 0, 0, 0);
+    //
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    //separacion de cajas: h=15 v=5
+    doc.roundedRect(baseX, 55, 104, 175, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(20, 68, `User Privileges: `);
+    //
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    doc.roundedRect(120, 55, 80, 15, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(125, 64, `ID: ${oUserToPrint.id}`);
+    //
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    doc.roundedRect(120, 75, 80, 15, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(125, 84, `Name: ${oUserToPrint.name}`);
+    //
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    doc.roundedRect(120, 95, 80, 15, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(125, 104, `Surname: ${oUserToPrint.surname}`);
+    //
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    doc.roundedRect(120, 115, 80, 15, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(125, 124, `Lastname: ${oUserToPrint.lastname}`);
+    //
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    doc.roundedRect(120, 135, 80, 15, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(123, 144, `Email: ${oUserToPrint.email}`);
+    //
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    doc.roundedRect(120, 155, 80, 15, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(125, 164, `Username: ${oUserToPrint.username}`);
+    //
+
+    var rol = "rol";
+    if (oUserToPrint.role == true) {
+      rol = "User";
+      doc.setFontSize(12);
+      doc.text(20, 78, `— Get own data `);
+      doc.text(20, 88, `— See threads `);
+      doc.text(20, 98, `— See replies `);
+      doc.text(20, 108, `— Create threads `);
+      doc.text(20, 118, `— Create replies `);
+
+
+    } else {
+      rol = "Admin";
+      doc.setFontSize(12);
+      doc.text(20, 78, `— Cosis cosis `);
+      doc.text(20, 88, `— Cosis cosis `);
+
+    }
+
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    doc.roundedRect(120, 175, 80, 15, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(125, 184, `Role: ${rol}`);
+    //
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    doc.roundedRect(120, 195, 80, 15, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(125, 204, `Threads: ${oUserToPrint.threads}`);
+    //
+    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+      parseInt(indigoPastel.substring(3, 5), 16),
+      parseInt(indigoPastel.substring(5, 7), 16));
+    doc.roundedRect(120, 215, 80, 15, 5, 5, 'F');
+    doc.setFontSize(14);
+    doc.text(125, 224, `Replies: ${oUserToPrint.replies}`);
+    //
+
+    return doc;
+  }
+
+  private pie(doc: any): any {
+
+    const indigoPastel = "#7887AB";
+
+    doc.setDrawColor(indigoPastel);
+    doc.line(10, 244, 200, 244);
+
+
     return doc;
   }
 
