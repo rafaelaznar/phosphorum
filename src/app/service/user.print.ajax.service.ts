cda3b2f95c23c22a5a8a7a6fc9b462931ebc813a
Nieves <smilenvs@gmail.com>
2023-12-15 18:11:32 +0100

    Contenido terminado

--- src/app/service/user.print.ajax.service.ts
+++ src/app/service/user.print.ajax.service.ts
@@ -65,98 +65,115 @@ export class UserPrintAjaxService {
     const indigoPastel = "#7887AB";
 
     doc.setTextColor(0, 0, 0, 0);
-    //
-    //Reordenar
-    doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
-      parseInt(indigoPastel.substring(3, 5), 16),
-      parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(baseX, 55, 104, 175, 5, 5, 'F');
-    doc.setFontSize(14);
-    doc.text(20, 68, `User Privileges: `);
+
     //
     doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
       parseInt(indigoPastel.substring(3, 5), 16),
       parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(120, 55, 80, 15, 5, 5, 'F');
+    doc.roundedRect(baseX, 55, 80, 15, 5, 5, 'F');
     doc.setFontSize(14);
-    doc.text(125, 64, `ID: ${oUserToPrint.id}`);
+    doc.text(20, 64, `ID: ${oUserToPrint.id}`);
     //
     doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
       parseInt(indigoPastel.substring(3, 5), 16),
       parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(120, 75, 80, 15, 5, 5, 'F');
+    doc.roundedRect(baseX, 75, 80, 15, 5, 5, 'F');
     doc.setFontSize(14);
-    doc.text(125, 84, `Name: ${oUserToPrint.name}`);
+    doc.text(20, 84, `Name: ${oUserToPrint.name}`);
     //
     doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
       parseInt(indigoPastel.substring(3, 5), 16),
       parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(120, 95, 80, 15, 5, 5, 'F');
+    doc.roundedRect(baseX, 95, 80, 15, 5, 5, 'F');
     doc.setFontSize(14);
-    doc.text(125, 104, `Surname: ${oUserToPrint.surname}`);
+    doc.text(20, 104, `Surname: ${oUserToPrint.surname}`);
     //
     doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
       parseInt(indigoPastel.substring(3, 5), 16),
       parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(120, 115, 80, 15, 5, 5, 'F');
+    doc.roundedRect(baseX, 115, 80, 15, 5, 5, 'F');
     doc.setFontSize(14);
-    doc.text(125, 124, `Lastname: ${oUserToPrint.lastname}`);
+    doc.text(20, 124, `Lastname: ${oUserToPrint.lastname}`);
     //
     doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
       parseInt(indigoPastel.substring(3, 5), 16),
       parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(120, 135, 80, 15, 5, 5, 'F');
-    doc.setFontSize(14);
-    doc.text(123, 144, `Email: ${oUserToPrint.email}`);
+    doc.roundedRect(baseX, 135, 80, 15, 5, 5, 'F');
+    doc.setFontSize(13);
+    doc.text(12, 144, `Email: ${oUserToPrint.email}`);
     //
     doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
       parseInt(indigoPastel.substring(3, 5), 16),
       parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(120, 155, 80, 15, 5, 5, 'F');
+    doc.roundedRect(baseX, 155, 80, 15, 5, 5, 'F');
     doc.setFontSize(14);
-    doc.text(125, 164, `Username: ${oUserToPrint.username}`);
+    doc.text(20, 164, `Username: ${oUserToPrint.username}`);
     //
 
     var rol = "rol";
     if (oUserToPrint.role == true) {
+      //
+      doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+        parseInt(indigoPastel.substring(3, 5), 16),
+        parseInt(indigoPastel.substring(5, 7), 16));
+      doc.roundedRect(100, 55, 100, 80, 5, 5, 'F');
+      doc.setFontSize(14);
+      doc.text(115, 68, `Privileges: `);
+      //
       rol = "User";
       doc.setFontSize(12);
-      doc.text(20, 78, `— Get own data `);
-      doc.text(20, 88, `— See threads `);
-      doc.text(20, 98, `— See replies `);
-      doc.text(20, 108, `— Create threads `);
-      doc.text(20, 118, `— Create replies `);
+      doc.text(125, 78, `— Get own data `);
+      doc.text(125, 88, `— See threads `);
+      doc.text(125, 98, `— See replies `);
+      doc.text(125, 108, `— Create threads `);
+      doc.text(125, 118, `— Create replies `);
 
 
     } else {
+      //
+      doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
+        parseInt(indigoPastel.substring(3, 5), 16),
+        parseInt(indigoPastel.substring(5, 7), 16));
+      doc.roundedRect(100, 55, 100, 130, 5, 5, 'F');
+      doc.setFontSize(14);
+      doc.text(115, 68, `Privileges: `);
+      //
       rol = "Admin";
       doc.setFontSize(12);
-      //Por terminar
-      doc.text(20, 78, `— Cosis cosis `);
-      doc.text(20, 88, `— Cosis cosis `);
+      doc.text(125, 78, `— Get all data `);
+      doc.text(125, 88, `— Delete all data`);
+      doc.text(125, 98, `— See threads `);
+      doc.text(125, 108, `— See replies `);
+      doc.text(125, 118, `— Create users `);
+      doc.text(125, 128, `— Create threads `);
+      doc.text(125, 138, `— Create replies `);
+      doc.text(125, 148, `— Edit users `);
+      doc.text(125, 158, `— Edit threads `);
+      doc.text(125, 168, `— Edit replies `);
+
 
     }
 
     doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
       parseInt(indigoPastel.substring(3, 5), 16),
       parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(120, 175, 80, 15, 5, 5, 'F');
+    doc.roundedRect(baseX, 175, 80, 15, 5, 5, 'F');
     doc.setFontSize(14);
-    doc.text(125, 184, `Role: ${rol}`);
+    doc.text(20, 184, `Role: ${rol}`);
     //
     doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
       parseInt(indigoPastel.substring(3, 5), 16),
       parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(120, 195, 80, 15, 5, 5, 'F');
+    doc.roundedRect(baseX, 195, 80, 15, 5, 5, 'F');
     doc.setFontSize(14);
-    doc.text(125, 204, `Threads: ${oUserToPrint.threads}`);
+    doc.text(20, 204, `Threads: ${oUserToPrint.threads}`);
     //
     doc.setFillColor(parseInt(indigoPastel.substring(1, 3), 16),
       parseInt(indigoPastel.substring(3, 5), 16),
       parseInt(indigoPastel.substring(5, 7), 16));
-    doc.roundedRect(120, 215, 80, 15, 5, 5, 'F');
+    doc.roundedRect(baseX, 215, 80, 15, 5, 5, 'F');
     doc.setFontSize(14);
-    doc.text(125, 224, `Replies: ${oUserToPrint.replies}`);
+    doc.text(20, 224, `Replies: ${oUserToPrint.replies}`);
     //
 
     return doc;
