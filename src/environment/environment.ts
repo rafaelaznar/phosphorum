bdf85817cfcb42cbae8df40e71f3363cfa074fb3
Rafael Aznar <rafaaznar@gmail.com>
2024-01-11 12:57:14 +0100

    arreglo puerto

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders } from "@angular/common/http";
 
-export const API_URL: string = 'http://localhost:8085';
+export const API_URL: string = 'http://localhost:8083';
 
 export const httpOptions = {
     headers: new HttpHeaders({
